526d67fc2ec1b88585d5935339368a13da8addc3
Juan Aguirre <j1927aguirre@gmail.com>
2021-02-15 15:53:17 -0500

    Add Add, Delete and List of Tasks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import { isEmpty } from "lodash";
-import { v4 as uuid } from "uuid";
+import React, { useState } from 'react';
+import { isEmpty, size } from 'lodash';
+import { v4 as uuid } from 'uuid';
 
 function App() {
-  const [task, setTask] = useState("");
+  const [task, setTask] = useState('');
   const [tasks, setTasks] = useState([]);
 
   const addTask = (e) => {
@@ -19,12 +19,13 @@ function App() {
 
     setTasks([...tasks, newTask]);
     setTask('');
-    console.table(tasks);
   };
 
-  const handleChange = (e) => {
-    setTask(e.target.value);
-    console.log(e.target.value);
+  const handleInputChange = (e) => setTask(e.target.value);
+
+  const handleDeleteTask = (id) => {
+    const newTasks = tasks.filter((t) => t.id !== id);
+    setTasks([...newTasks]);
   };
 
   return (
@@ -34,21 +35,31 @@ function App() {
       <div className="row">
         <div className="col-8">
           <h2 className="text-center">Tasks</h2>
-          <ul className="list-group">
-            {
-              tasks.map((task) => (
+          {
+            size(tasks) > 0 ? (
+              <ul className="list-group">
+              {tasks.map((task) => (
                 <li className="list-group-item" key={task?.id}>
                   <span className="lead">{task?.name}</span>
-                  <button className="btn btn-sm btn-danger float-right ml-2 rounded-pill">
+                  <button
+                    onClick={() => handleDeleteTask(task.id)}
+                    className="btn btn-sm btn-danger float-right ml-2 rounded-pill"
+                  >
                     Delete
                   </button>
                   <button className="btn btn-sm btn-warning float-right rounded-pill">
                     Edit
                   </button>
                 </li>
-              ))
-            }
-          </ul>
+              ))}
+            </ul>
+            ) : (
+              <div className="jumbotron">
+                <h3 className="display-4 text-center">No scheduled tasks!</h3>
+              </div>
+            )
+            
+          }
         </div>
         <div className="col-4">
           <h2 className="text-center">Form</h2>
@@ -56,7 +67,7 @@ function App() {
             <input
               type="text"
               className="form-control mb-2"
-              onChange={handleChange}
+              onChange={handleInputChange}
               placeholder="Add a task..."
               value={task}
             />
